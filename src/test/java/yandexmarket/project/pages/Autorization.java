8ab15ddf68f0cdf97be2752dd7643006bcb028b3
diana02092002 <99760136+diana02092002@users.noreply.github.com>
2022-04-04 21:24:05 +0300

    Create Selenium

--- src/test/java/yandexmarket/project/pages/Autorization.java
+++ src/test/java/yandexmarket/project/pages/Autorization.java
@@ -2,28 +2,34 @@ package yandexmarket.project.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import yandexmarket.Utils.ConfigFileReader;
 
 public class Autorization {
+    private ConfigFileReader configFileReader=ConfigFileReader.configFileReader;
     private WebDriver driver;
+
 
     public Autorization(WebDriver driver) {
         this.driver = driver;
     }
 
+
     private By login = By.xpath("//*[@id=\"passp-field-login\"]");
     public void login() {
-        driver.findElement(login).sendKeys("babijdiana@yandex.by");
+        driver.findElement(login).sendKeys(configFileReader.getLogin());
     }
     private By loginClick = By.xpath("//*[@id=\"passp:sign-in\"]");
-    public void loginClickButton(){
+    public void loginClick(){
         driver.findElement(loginClick).click();
     }
-    private By passwordIn = By.xpath("//*[@id=\"passp-field-passwd\"]");
-    public void passwordInput(){
-        driver.findElement(passwordIn).sendKeys("diana020902");
+    private By password = By.xpath("//*[@id=\"passp-field-passwd\"]");
+    public void password(){
+        driver.findElement(password).sendKeys(configFileReader.getPassword());
     }
     private By passwordClick = By.xpath("//*[@id=\"passp:sign-in\"]");
-    public void passwordClickButton(){
+    public void passwordClick(){
         driver.findElement(passwordClick).click();
     }
 }
+
+
--- src/test/java/yandexmarket/project/tests/BaseTest.java
+++ src/test/java/yandexmarket/project/tests/BaseTest.java
@@ -1,14 +1,22 @@
 package yandexmarket.project.tests;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+import yandexmarket.Utils.ConfigFileReader;
 import yandexmarket.framework.Browser;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    private ConfigFileReader configFileReader=ConfigFileReader.configFileReader;
+
     @BeforeMethod
     public void before() {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\MSI\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        Browser.getDriver().manage().window().maximize();
+        System.setProperty(configFileReader.getDriverAdapter(),configFileReader.getDriverPath());
+        WebDriver driver= Browser.getDriver();
+        driver.manage().timeouts().implicitlyWait(10,
+                TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
-
 }
 
